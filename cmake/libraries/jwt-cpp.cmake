3feca2045f1f353cf3d3a8f2b104368c92f27d1a
geramyloveless <gloveless@jqluv.com>
2026-03-12 22:56:34 -0700

    Fix Windows CI: locate OpenSSL .lib files in choco's non-standard paths
    Choco's OpenSSL package places .lib files in lib/VC/x64/MD/ which
    FindOpenSSL doesn't search by default. Explicitly locate libcrypto.lib
    and libssl.lib and pass them to CMake. Also suppress CMP0169 warning
    for jwt-cpp FetchContent_Populate (EXCLUDE_FROM_ALL needs CMake 3.28+).

--- cmake/libraries/jwt-cpp.cmake
+++ cmake/libraries/jwt-cpp.cmake
@@ -5,9 +5,18 @@ FetchContent_Declare(
         GIT_REPOSITORY https://github.com/Thalhammer/jwt-cpp.git
         GIT_TAG v0.7.0
 )
-# Use EXCLUDE_FROM_ALL to prevent jwt-cpp's install() targets from polluting packages
+# Use EXCLUDE_FROM_ALL to prevent jwt-cpp's install() targets from polluting packages.
+# FetchContent_MakeAvailable doesn't support EXCLUDE_FROM_ALL until CMake 3.28,
+# so we use the manual Populate + add_subdirectory approach (suppress CMP0169 warning).
+cmake_policy(GET CMP0169 _cmp0169)
+if(POLICY CMP0169)
+    cmake_policy(SET CMP0169 OLD)
+endif()
 FetchContent_GetProperties(jwt-cpp)
 if(NOT jwt-cpp_POPULATED)
     FetchContent_Populate(jwt-cpp)
     add_subdirectory(${jwt-cpp_SOURCE_DIR} ${jwt-cpp_BINARY_DIR} EXCLUDE_FROM_ALL)
+endif()
+if(_cmp0169)
+    cmake_policy(SET CMP0169 ${_cmp0169})
 endif()
